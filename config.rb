0c7be2c8a1944d762cea94abcf2c2386e91190e0
Josh Dean <github@designerdean.com>
2012-12-07 17:55:13 -0700

    I just want one css-related directory on the root yo

--- config.rb
+++ config.rb
@@ -3,7 +3,7 @@
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "stylesheets"
-sass_dir = "sass"
+sass_dir = "stylesheets/sass"
 images_dir = "images"
 javascripts_dir = "javascripts"
 
@@ -14,7 +14,7 @@ javascripts_dir = "javascripts"
 # relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 
 
 # If you prefer the indented syntax, you might want to regenerate this
