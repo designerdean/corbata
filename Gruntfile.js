382d36516dd3eceec87f0458b8e50b9734bf4ec4
Josh Dean <josh@designerdean.com>
2014-03-12 15:00:07 -0600

    use Grunt in master instead of Ruby

---
+++ Gruntfile.js
@@ -0,0 +1,124 @@
+module.exports = function (grunt) {
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    // CONFIG =================================== /
+
+    // Compile Compass/Sass
+    compass: {
+      dist: {
+        options: {
+          sassDir: ['stylesheets/sass'],
+          cssDir: ['stylesheets']
+        }
+      }
+    },
+
+    // Compile CoffeeScript
+    coffee: {
+      compile: {
+        files: {
+          'javascripts/main.js': ['javascripts/coffee/*.coffee']
+        }
+      }
+    },
+
+    // Modernizr
+    modernizr: {
+      "devFile" : "javascripts/vendor/modernizr-dev.js",
+      "outputFile" : "javascripts/vendor/modernizr-custom.js",
+      // Based on default settings on http://modernizr.com/download/
+      "extra" : {
+          "shiv" : true,
+          "printshiv" : false,
+          "load" : true,
+          "mq" : false,
+          "cssclasses" : true
+      },
+      // Based on default settings on http://modernizr.com/download/
+      "extensibility" : {
+          "addtest" : false,
+          "prefixed" : false,
+          "teststyles" : false,
+          "testprops" : false,
+          "testallprops" : false,
+          "hasevents" : false,
+          "prefixes" : false,
+          "domprefixes" : false
+      },
+      "uglify" : true,
+      // Crawl project for references to Modernizr tests - it will crawl all *.js, *.css, *.scss files, except files that are in node_modules/
+      "parseFiles" : true,
+      // "matchCommunityTests" : false
+    },
+
+    // Grunt Watch
+    watch: {
+      options: {
+        livereload: true
+      },
+      stylesheets: {
+        files: ['stylesheets/sass/*.scss'],
+        tasks: ['compass', 'autoprefixer']
+      },
+      javascripts: {
+        files: ['javascripts/coffee/*.coffee'],
+        tasks: ['coffee']
+      },
+      html: {
+        files: ['*.html']
+      },
+      modernizr: {
+        files: ['stylesheets/sass/*.scss', 'javascripts/vendor/*.js'],
+        tasks: ['modernizr']
+      },
+    },
+
+    // Autoprefixer
+    autoprefixer: {
+      options: {
+        // Task-specific options go here.
+      },
+      // prefix all files
+      multiple_files: {
+        expand: true,
+        flatten: true,
+        src: 'stylesheets/*.css',
+        dest: 'stylesheets/'
+      },
+    },
+
+    // Start Local Server
+    connect: {
+      livereload: {
+        options: {
+          livereload: true,
+          port: 9000
+        }
+      }
+    },
+
+  });
+
+  // DEPENDENT PLUGINS ========================= /
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks("grunt-modernizr");
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-autoprefixer');
+
+
+  // TASKS ===================================== /
+
+  grunt.registerTask('default', [
+    'connect',
+    'compass',
+    'coffee',
+    'modernizr',
+    'autoprefixer',
+    'watch']);
+
+};
--- README.md
+++ README.md
@@ -5,6 +5,11 @@ Corbata is a base layer of rules and conventions that act as a starting point fo
 
 > a bumper compendium of cascading and connected CSS files, naming conventions, modules, plugins and library scripts that ensure any project led or worked on by any member(s) of a team will stay on convention, and be simpler for anyone else to step into and work with at any time.
 
-Outside of the common (and some custom) CSS resets, Corbata does not have any default styling. It's just meant to act as a starting place for responsive and organized HTML as well as DRY, readable, and modular CSS (through Sass and Compass). Corbata is similar to, and inspired by, [HTML5 Boilerplate](http://html5boilerplate.com).
+Outside of the common (and some custom) CSS resets, Corbata does not have any default styling. It's just meant to act as a starting place for responsive and organized HTML as well as DRY, readable, and modular CSS (through Sass and Compass). It also allows for CoffeeScript. Corbata is similar to, and inspired by, [HTML5 Boilerplate](http://html5boilerplate.com).
 
-You can also [check out the the Grunt version of Corbata](https://github.com/designerdean/corbata/tree/grunt).
+This version of Corbata uses [Grunt](http://gruntjs.com), a JavaScript task runner, to make automation easier. I've only set it up for compiling Sass/Compass and CoffeeScript, but [it can do a whole lot more than that](http://gruntjs.com/plugins). So, you will need to install a few things:
+
+- [Node & NPM](http://nodejs.org)
+- [Grunt](http://gruntjs.com/getting-started)
+
+After everything is installed, open the terminal and cd into the directory where you have placed Corbata. Then, simply run "grunt". This will watch and automatically compile all the Sass and CoffeeScript files into CSS and JavaScript. In the Gruntfile, I'm using port 9000 to serve up the app; so, in order for livereload to work, you'll need to navigate to localhost:9000.
